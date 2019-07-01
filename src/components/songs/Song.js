f5788da1209eaf584931fdf36b287558076127c4
Luis Zabala <luis.zabala076@gmail.com>
2019-07-01 10:43:53 -0400

    branched to redux build

--- src/components/songs/Song.js
+++ src/components/songs/Song.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SongsContainer from './containers/SongsContainer';
 
 class Song extends Component {
+
   handleOnClick() {
     this.props.deleteSong(this.props.song.id)
   }
@@ -13,7 +14,7 @@ class Song extends Component {
 
       <div>
         <li>
-          {song}
+          {song.text}
           <button onClick={() => this.handleOnClick()}> Delete </button>
           <SongsContainer song={song}/>
         </li>
